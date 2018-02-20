b3742e7ea074f5ebd819afd2665b898d376bf0bc
Jon Christensen <jon@jychri.com>
2018-02-19 19:22:09 -0600

    this one weird trick...drs hate him...

--- workspace.js
+++ workspace.js
@@ -40,43 +40,74 @@ function getCalendarByName(name) {
   return CalendarApp.getCalendarsByName(name)[0];
 } 
 
-function singleDayCalendarEventWithAttachment(rsp, cal, file) {
-  var id = cal.getId();
-  start  = dateObjectFromDateAndTime(rsp.Date, rsp.Start);
-  end    = dateObjectFromDateAndTime(rsp.Date, rsp.End);
+// function singleDayCalendarEventWithAttachment(rsp, cal, file) {
+//   var id = cal.getId();
+//   start  = dateObjectFromDateAndTime(rsp.Date, rsp.Start);
+//   end    = dateObjectFromDateAndTime(rsp.Date, rsp.End);
 
- var eventObj = {
-    summary: 'Space X Launch',
-    location: 'SPACE',
-    description: 'They put a car in space?!',
-    start: {dateTime: start.toISOString()},
-    end: {dateTime: end.toISOString()},
-    attachments: [{
-        'fileUrl': "https://docs.google.com/document/d/1Kl3uXKHNLW5SuFhN3v2TpnE7rDbkWhx96mlynVPV42A/edit",
-        'title': 'Attached Document'}, 
-      {'fileUrl': "https://drive.google.com/open?id=15boI1XDEkBfwf2BHQOMrbWH66PxfC0ey",
-        'title': 'Star Man'}
-    ]
-  };
+//  var eventObj = {
+//     summary: 'Space X Launch',
+//     location: 'SPACE',
+//     description: 'They put a car in space?!',
+//     start: {dateTime: start.toISOString()},
+//     end: {dateTime: end.toISOString()},
+//     attachments: [{
+//         'fileUrl': "https://docs.google.com/document/d/1Kl3uXKHNLW5SuFhN3v2TpnE7rDbkWhx96mlynVPV42A/edit",
+//         'title': 'Attached Document'}, 
+//       {'fileUrl': "https://drive.google.com/open?id=15boI1XDEkBfwf2BHQOMrbWH66PxfC0ey",
+//         'title': 'Star Man'}
+//     ]
+//   };
 
-  var create = Calendar.Events.insert(eventObj, id, {"supportsAttachments" : true});
+//   var create = Calendar.Events.insert(eventObj, id, {"supportsAttachments" : true});
+// }
+
+function dateObjectFromDateAndTime(date, time) {
+  return new Date (date + "," + time);
+} 
+
+// calendar = form
+// summary  = title
+// location = location
+
+function dateFromFormResponse(time, date) {
+  var hours   = time.split(":")[0];
+  var minutes = time.split(":")[1];
+  var result  = new Date (date);
+  result.setHours(hours);
+  result.setMinutes(minutes);
+  return result;
 }
 
-function onResponseCreateEventWithAttachment() {
+function createSingleDayCalendarEventWithAttachment(rsp, cal, file) {
+  var id    = cal.getId();
+  var startISO = dateFromFormResponse(rsp.start, rsp.date).toISOString();
+  var endISO   = dateFromFormResponse(rsp.end, rsp.date).toISOString();
+  var event = {
 
-  // in production, this will actually come from the form
-  // ...but for now, let's just test 
-
-  var obj = {
-    Date:  "2/6/2018",
-    Start: "8:30:00 AM",
-    End:   "05:00:00 PM",
-    Description: "Neat!"
+    // start: {dateTime: start.toISOString()},
+    // end: {dateTime: end.toISOString()},
+    start: {dateTime: startISO},
+    end: {dateTime: startISO},
+    summary: rsp.title,
+    location: rsp.location,
+    description: rsp.description,
+    // attachments: [{
+    //   fileUrl: file.getUrl(),
+    //   title: file.getTitle()
+    // }]
   };
 
+  Logger.log(event);
+
+  Calendar.Events.insert(event, id, {"supportsAttachments" : true});
+}
+
+function testing() {
   var form = FormApp.getActiveForm();
-  Logger.log(lastFormResponseAsObject(form));
   var cal  = getCalendarByName("Development");
   var file = DriveApp.getFileById("1Kl3uXKHNLW5SuFhN3v2TpnE7rDbkWhx96mlynVPV42A");
-  singleDayCalendarEventWithAttachment(obj, cal, file);
+  var rsp  = lastFormResponseAsObject(form);
+  Logger.log(rsp);
+  createSingleDayCalendarEventWithAttachment(rsp, cal, file);
 } 
